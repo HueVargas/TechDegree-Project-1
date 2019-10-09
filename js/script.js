09874f32cafa2f317ef2291d49f590c729676ccd
Hue V <huevargas@gmail.com>
2019-10-09 10:36:07 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,26 +18,35 @@ let quotes = [
   {
     quote: "Success in creating AI would be the biggest event in human history. Unfortunately, it might also be the last, unless we learn how to avoid the risks.",
     source: "Stephan Hawking",
+    citation: "https://www.cnbc.com/2017/11/06/stephen-hawking-ai-could-be-worst-event-in-civilization.html",
+    years: "1942 - 2018"
   },
 
   {
     quote: "One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man.",
     source: "Elbert Hubbard",
+    citation: "https://www.brainyquote.com/quotes/elbert_hubbard_108045",
+    years: "June 19, 1856 – May 7, 1915"
   },
 
   {
     quote: "I'm having nightmares that i forgot to back up my system",
     source: "Richard Hendricks",
+    citation: "Silicon Valley TV Series"
   },
 
   {
     quote: "Science and technology revolutionize our lives, but memory, tradition and myth frame our response.",
     source: "Arthur M. Schlesinger",
+    citation: "https://www.brainyquote.com/quotes/arthur_m_schlesinger_109503",
+    years: "Feb 27, 1888 - Oct 30, 1965"
   },
 
   {
     quote: "Every human has four endowments - self awareness, conscience, independent will and creative imagination. These give us the ultimate human freedom... The power to choose, to respond, to change.",
     source: "Stephen Covey",
+    citation: "https://www.thequotablecoach.com/every-human-has-four-endowments-self-awareness-conscience-independent-will-and-creative-imagination-these-give-us-the-ultimate-human-freedom-the-power-to-choose-to-respond/",
+    years: "October 24, 1932 – July 16, 2012"
   },
 ];
 
@@ -48,7 +57,11 @@ let quotes = [
    - Create a variable to store a random number 
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
+function getRandomQuote() {
+  let quoteList = Math.floor(Math.random() * quotes.length);
+  return quotes[quoteList];
 
+}
 
 
 
@@ -64,8 +77,19 @@ let quotes = [
    - Don't forget to close that final `p` tag.
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
+function printQuote() {
+  let result = getRandomQuote();
+  let stringHTML = "";
+  stringHTML = "<p class='quote'>" + result.quote + "</p>";
+  stringHTML += "<p class='source'>" + result.source;
 
+if ("citation" in result) {
+  stringHTML += "<span class='citation'>" + result.citation + "</span>";
+}
 
+  document.getElementById('quote-box').innerHTML = stringHTML;
+}
+printQuote();
 
 
 /***
